9fe0aaf9a29d80be0c5aec614a007eda0bb3fbe7
Kirill Antonov <kirill50027@gmail.com>
2023-04-04 21:49:25 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println("Сравнение бинов helloworld: " + (bean == beanTwo));
+        Cat catOne = applicationContext.getBean("cat", Cat.class);
+        Cat catTwo = applicationContext.getBean("cat", Cat.class);
+        System.out.println("Сравнение бинов cat: " + (catOne == catTwo));
     }
 }
